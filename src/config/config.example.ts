214ea37affa3cb7b586afbd58fd4e62a91c55b4e
Marta Brylska <tomica.marta@gmail.com>
2023-11-07 11:33:30 +0100

    added configuration for deployment

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -4,5 +4,10 @@ dbHost: 'localhost',
 dbUser: 'root',
 dbPassword: 'password',
 dbDatabase: 'database',
+corsOrigin: 'http://localhost:3000',
 };
+
+ export const jwtConstant = {
+    secret: 'your_secret_jwt_key'
+}
  **/
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import {ValidationPipe} from "@nestjs/common";
 import helmet from "helmet";
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import {config} from "./config/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
     credentials: true,
   });
 
   app.use(cookieParser());
+
   // (app as NestExpressApplication).use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     // disableErrorMessages: true,
