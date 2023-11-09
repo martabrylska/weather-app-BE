ad4980f09722b480179bf8f3719019e5ab7d2c56
Marta Brylska <tomica.marta@gmail.com>
2023-11-09 12:02:12 +0100

    changed config example

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,8 +1,13 @@
-/**
- export const config = {
-dbHost: 'localhost',
-dbUser: 'root',
-dbPassword: 'password',
-dbDatabase: 'database',
+/*
+export const config = {
+    dbHost: 'localhost',
+    dbUser: 'root',
+    dbPassword: 'password',
+    dbDatabase: 'database',
+    corsOrigin: 'http://localhost:3000',
 };
- **/
+
+export const jwtConstant = {
+    secret: 'your_secret_jwt_key'
+}
+*/
